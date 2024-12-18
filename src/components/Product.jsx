e514e1977df1a371a2ffcb98650fd4937803c67a
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2024-12-18 22:15:04 +0530

    alignment for responsive

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 const Product = ({item}) => {
   return (
-    <div className='mx-auto px-8 border border-gray-200'>
-        <img src={item.image} alt="Missing image"/>
-        <p className='text-xl'>{item.name}</p>
-        <p className='text-lg'>${item.price}</p>
+    <div className='mx-auto px-8 border border-gray-200 rounded-lg'>
+        <img src={item.image} alt="glasses"/>
+      
+        <div className='flex flex-row items-center justify-between py-4'>
+          <div>
+          <p className='text-xl'>{item.name}</p>
+          <p className='text-lg'>${item.price}</p>
+          </div>
+          <button className='bg-indigo-500 text-white text-lg font-medium rounded-full p-4'>Add to Basket </button>
+        </div>
+       
     </div>
   )
 }
